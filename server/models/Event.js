8002f714b72f8910dd9476c2cc31253e16cfceae
chaimasammoud <chaymaa.sammoud@gmail.com>
2025-04-05 23:52:08 +0000

    init

---
+++ server/models/Event.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const eventSchema = new mongoose.Schema({
+  type: {
+    type: String,
+    enum: ['Évenement', 'Réunion', 'Tâche'],
+    required: true
+  },
+  titre: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String
+  },
+  date_debut: {
+    type: Date,
+    required: true
+  },
+  date_fin: {
+    type: Date,
+    required: true
+  },
+  emplacement: {
+    type: String
+  },
+  organisateur_id: {
+    type: Schema.Types.ObjectId,
+    ref: 'Utilisateurs',
+    required: true
+  },
+  isRecurring: {
+    type: Boolean,
+    default: false
+  },
+  type_recurrence: {
+    type: String,
+    enum: ['daily', 'weekly', 'monthly', 'none'],
+    default: 'none'
+  },
+  status: {
+    type: String,
+    enum: ['En_attente', 'Confirme', 'Annule', 'Terminé'],
+    default: 'En_attente'
+  },
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('Event', eventSchema);
