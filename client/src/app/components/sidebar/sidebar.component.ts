f67cd4f689fb1afd821388e7560cc9de82473d29
Malek.Ferjani <malek.ferjani@sinorfi.com>
2025-05-17 15:24:46 +0100

    project admin module

---
+++ client/src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {RouterLink, RouterLinkActive} from '@angular/router';
+
+@Component({
+  selector: 'app-sidebar',
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.css'
+})
+export class SidebarComponent {
+
+}
